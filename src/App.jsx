f21b9acd507773ea157280605362d2d2f43608c6
elmurodvokhidov <elmurodvkh@gmail.com>
2024-03-12 21:57:08 +0500

    lesson1

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl underline text-green-300 bg-red-500 w-fit md:bg-blue-500 hover:text-3xl animate-bounce">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
