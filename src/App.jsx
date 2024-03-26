2618073f9ef0968bdfdb11657cf435cec06156f9
elmurodvokhidov <elmurodvkh@gmail.com>
2024-03-26 15:58:38 +0500

    add new todo and delete todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import { useContext } from "react"
 import { Context } from "./context/ContextComponent";
 
 function App() {
-  const { todos } = useContext(Context);
+  const {
+    todos,
+    newTodo,
+    getInputValue,
+    handleCreate,
+    handleDelete,
+  } = useContext(Context);
 
   return (
     <div className="flex items-center justify-center bg-gray-100 h-screen">
@@ -10,8 +16,18 @@ function App() {
         <h1 className="text-4xl uppercase text-center font-bold">To Do List</h1>
 
         <div className="flex flex-col justify-center items-start gap-4">
-          <input type="text" name="" id="" placeholder="Enter a task" className=" text-xl p-2 w-full rounded border border-gray-400" />
-          <button className="bg-gray-100 border border-black rounded capitalize px-4 py-1">add task</button>
+          <input
+            onChange={getInputValue}
+            type="text"
+            name="title"
+            value={newTodo.title}
+            id="title"
+            placeholder="Enter a task"
+            className=" text-xl p-2 w-full rounded border border-gray-400" />
+          <button
+            type="button"
+            onClick={handleCreate}
+            className="bg-gray-100 border border-black rounded capitalize px-4 py-1">add task</button>
         </div>
 
         <div className="max-h-80 overflow-y-auto flex flex-col gap-2 pt-2">
@@ -21,7 +37,9 @@ function App() {
                 <h1 className="text-xl">{todo.title}</h1>
                 <div className="flex gap-2">
                   <button className="rounded-md capitalize py-2 px-4 bg-blue-500 text-white">edit</button>
-                  <button className="rounded-md capitalize py-2 px-4 bg-blue-500 text-white">delete</button>
+                  <button
+                    onClick={() => handleDelete(todo.id)}
+                    className="rounded-md capitalize py-2 px-4 bg-blue-500 text-white">delete</button>
                 </div>
               </div>
             ))
--- src/context/ContextComponent.jsx
+++ src/context/ContextComponent.jsx
@@ -5,20 +5,57 @@ export const Context = createContext();
 
 export default function ContextFunction({ children }) {
     const [todos, setTodos] = useState([]);
+    const [newTodo, setNewTodo] = useState({
+        title: ""
+    });
 
-    useEffect(() => {
+    const getInputValue = (e) => {
+        setNewTodo({
+            ...newTodo,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function getData() {
         axios("http://localhost:5000/todos")
             .then((response) => {
                 setTodos(response.data);
             })
             .catch((error) => console.log(error))
+    };
 
+    useEffect(() => {
+        getData();
     }, []);
+
+    const handleCreate = async function () {
+        try {
+            await axios.post("http://localhost:5000/todos", newTodo);
+            setNewTodo({
+                title: ""
+            });
+            getData();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            await axios.delete(`http://localhost:5000/todos/${id}`);
+            getData();
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
         <Context.Provider value={{
             todos,
-            setTodos,
+            newTodo,
+            getInputValue,
+            handleCreate,
+            handleDelete,
         }}>
             {children}
         </Context.Provider>
